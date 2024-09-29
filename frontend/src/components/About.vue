6ec76c3acf6505ef11543a1b9b5c674dffd078c5
wanglin <wanglin@ihep.ac.cn>
2024-09-29 16:10:21 +0800

    Add changelog and version number to the about page

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -3,8 +3,9 @@
 	<div class="grid">
 		<div class="col-12">
 			<div class="card">
+				<h3 style="font-weight: bold; color: RGB(29,149,243); text-align: center;">Version 2.2.0</h3>
 				<h5 style="font-weight: bold; color: RGB(29,149,243);">Introduction</h5>
-				<p>Clog (Compact Electronic Logbook System) is a web-based application designed to be used for accelerator projects.</p>
+				<p>Clog (Compact Electronic Logbook System) is a web-based application designed to be used for accelerator facilities.</p>
 				<h5 style="font-weight: bold; color: RGB(29,149,243);">Copyright</h5>
 				<p>This software is Copyright by Institute of High Energy Physics (IHEP) (c) Copyright 2022.<br><br>
 					You may use this software under the terms of the GNU public license (GPL). <br>
@@ -18,6 +19,68 @@
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Author: Lin Wang<br>
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email: wanglin@ihep.ac.cn<br>
 				</p>
+				<h5 style="font-weight: bold; color: RGB(29,149,243);">Changelog</h5>
+<pre>
+#### v2.2.0
+
+- Add Docker deployment support
+- Change the build tool from Vue CLI to Vite
+- Add email notification support
+- Add rich text support with ckeditor5
+- Add log template design function
+- Add log template import function
+- Add log cache function with the Browser localStorage
+
+#### v2.1.2
+
+- Update some dependencies on frontend to remove vulnerabilities
+- Update some dependencies on backend to remove vulnerabilities
+- Replace "localhost" in MongoDB URL which does not work in Node.js 18 with "127.0.0.1"
+
+#### v2.1.1
+
+- In order to provide image left/right navigation function, Replaced PrimeVue "Image" component with Element Plus "el-image" component on LogDetail page, which is shared by Xiaokang Sun from HALF project.
+
+#### v2.1.0
+
+- Significant change: move attachment storage from MongoDB to file system due to 16MB limitation and performance issue
+- Added attachment icons for word, excel, powerpoint and mp4 files
+- Fixed the issue that logs cannot be edited when attachments is empty
+- Fixed the issue that image toolbar is hidden behind large image in preview mode
+- Removed the history author expansion part for findLogs() since it seriously affect the performance
+
+#### v2.0.5
+
+- Add logbook admin function
+- Truncate log description at 50 characters on logbook page
+- Sort logs by createdAt instead of updatedAt
+
+#### v2.0.4
+
+- Fixed the issue that log data appends to inactive logs
+- Adjusted locale for zh
+- Fixed the datatable index issue
+- Fixed the '$facet exceeds the limit of 104857600 bytes' issue
+
+#### v2.0.3
+
+- Added API token append for attachments
+
+#### v2.0.2
+
+- Added API token generation dialog and log append function
+
+#### v2.0.1
+
+- Updates versions of Node.js, Vue.js and PrimeVue
+
+#### v2.0.0
+
+**Initial version:**
+
+- Upgrades Clog from v1.0 to v2.0
+- Replaces technical sulution from JavaEE/MySQL to Vue.js/Node.js/MongoDB
+</pre>
 			</div>
 		</div>
 	</div>
